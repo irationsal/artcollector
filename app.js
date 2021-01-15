dd6a8947617df333f3ed11f5456743d377a785a6
irationsal <irationsal@gmail.com>
2021-01-14 17:49:49 -0800

    fixed i think

--- app.js
+++ app.js
@@ -318,18 +318,18 @@ $('#preview').on('click', '.object-preview', function (event) {
 });
 
 $('#feature').on('click', 'a', async function (event) {
+  const href = $(this).attr('href')
+
   if (href.startsWith('mailto'))
     return
-  else {
     // read href off of $(this) with the .attr() method
-    const SEARCH_URL = $(this).attr('href')
     //console.log("searchURL",SEARCH_URL)
     // prevent default
     event.preventDefault()
     // call onFetchStart
     try {
       onFetchStart()
-      const response = await fetch(SEARCH_URL)
+      const response = await fetch(href)
       const data = await response.json()
       console.log(data)
       updatePreview(data.records, data.info)
